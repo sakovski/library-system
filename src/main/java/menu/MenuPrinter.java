70774f05fe90a1263e3096c05ee0a89313c60e0b
sakovski <sakasew@gmail.com>
2018-05-17 23:56:38 +0200

    Fixed StringBuilder bug in MenuPrinter.java

--- src/main/java/menu/MenuPrinter.java
+++ src/main/java/menu/MenuPrinter.java
@@ -8,17 +8,27 @@ public class MenuPrinter {
     public static final String HEADER_MESSAGE = "Please type a command from command list: ";
 
     private final MenuChoices menuChoices = new MenuChoices();
-    private final StringBuilder stringBuilder = new StringBuilder();
+    private final StringBuilder stringBuilder;
+
+    public MenuPrinter() {
+        this.stringBuilder = this.constructStringBuilder();
+    }
 
     public void printMenu() {
-        stringBuilder.append(HEADER_MESSAGE).append(System.lineSeparator());
-        List<String> menuChoicesCommands = menuChoices.getMenuChoicesCommands();
-        menuChoicesCommands.stream()
-                .forEach(s -> stringBuilder.append(s).append(System.lineSeparator()));
         System.out.print(stringBuilder.toString());
     }
 
     public void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
     }
+
+    private StringBuilder constructStringBuilder() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(HEADER_MESSAGE).append(System.lineSeparator());
+        List<String> menuChoicesCommands = menuChoices.getMenuChoicesCommands();
+        menuChoicesCommands.stream()
+                .forEach(s -> stringBuilder.append(s).append(System.lineSeparator()));
+        return stringBuilder;
+    }
+
 }
